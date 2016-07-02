5113d8a7f1e1bcc91dfd61cae0677a7b3913f33f
othmaan <othmaan@users.noreply.github.com>
2016-07-02 03:07:52 +0200

    - Correcting imports

--- server/load.js
+++ server/load.js
@@ -21,12 +21,12 @@ function modifyCheckoutWorkflow() {
 if (ReactionCore && ReactionCore.Hooks) {
   ReactionCore.Hooks.Events.add("onCoreInit", () => {
     ReactionCore.Log.info("Initialize using Bees Knees Data");
-    ReactionImport.fixture().process(Assets.getText("private/data/Shops.json"), ["name"], ReactionImport.shop);
+    ReactionImport.fixture().process(Assets.getText("data/Shops.json"), ["name"], ReactionImport.shop);
     // ensure Shops are loaded first.
     ReactionImport.flush(ReactionCore.Collections.Shops);
     // these will flush/import with the rest of the imports from core init.
-    ReactionImport.fixture().process(Assets.getText("private/data/Products.json"), ["title"], ReactionImport.load);
-    ReactionImport.fixture().process(Assets.getText("private/data/Tags.json"), ["name"], ReactionImport.load);
+    ReactionImport.fixture().process(Assets.getText("data/Products.json"), ["title"], ReactionImport.load);
+    ReactionImport.fixture().process(Assets.getText("data/Tags.json"), ["name"], ReactionImport.load);
     ReactionImport.flush();
   });
 
